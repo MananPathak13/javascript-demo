fa0aaac82f27382643be4d9aee732ed4995bcf24
Manan Pathak <pathakmanan5@gmail.com.com>
2024-04-21 19:35:43 -0400

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "cidr" {
+  default = "44.55.66.77/32"
+}
+
+variable "instance_name" {}
+
+variable "port1" {
+  type = number
+}
+variable "port2" {
+  type = number
+}
+variable "port3" {
+  type = number
+}
+variable "port4" {
+  type = number
+}
